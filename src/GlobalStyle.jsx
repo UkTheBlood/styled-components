feae6bd2424c87eff1980133e67931d694dce7d8
jaeuk Lee <lskkhj1005@naver.com>
2023-02-09 15:36:08 +0900

    styled components

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`          // createGlobalStyle API 사용
+    body {
+        font-family: "Helvetica", "Arial", sans-serif;
+        line-height: 1.5;
+    }
+`
+
+export default GlobalStyle
